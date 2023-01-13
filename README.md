edd7ecf0958e27c3b9833a0e6e38779c6981937f
ahmadmulyana13 <ahmad.mulyana15@yahoo.com>
2023-01-13 15:27:56 +0700

    f

---
+++ README.md
@@ -0,0 +1,1 @@
+# singgasanabaca13
---
+++ data.js
@@ -0,0 +1,33 @@
+const nama = "ahmad mulyana";
+let usia = 10;
+
+const biodata = document.getElementById('biodata');
+
+function generateBiodata() {
+    let generasi;
+
+    if(usia > 10 && usia < 18) { 
+         generasi = "generasi remaja";
+    } 
+    else if(usia > 18 && usia <30) {
+        generasi = "generasi dewasa";
+    } 
+    else if(usia >= 30) {
+        generasi = "ah kolot siamahhh anyengg👴👴👴👴👴";
+    }
+    else if(usia < 10 && usia > 2) {
+        generasi = "generasi anak anak";
+
+    }
+    else {
+        generasi = "generasi bayi"
+    }
+
+   return biodata.innerHTML = generasi;
+
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
